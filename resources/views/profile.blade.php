1a2d36d1448966b076b08dfb0b0bc0b72c996d1d
YuYX <yuyongxue@gmail.com>
2022-07-31 21:56:40 +0800

    5th commit from VSCode

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,29 +4,34 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-3">
-            <img class="rounded-circle" 
-            width="150" 
-            src="/storage/{{ $profile->image }}" alt="">
-        </div>
 
-        <div class="col-md-8">
-            <div class="card"> 
-                <strong>Hello {{$user->name}}</strong>
-                <span>You have <strong>{{$numPosts}}</strong> posts</span>
-                <div class="pt-3">{{ $profile->description }}</div>
-                <div class="pt-3"> 
-                    <a href="/profile/edit">Edit profile</a>
-                </div>
-            </div>
-        </div>
+            <div class="card" > 
+                <img class="rounded card-img-top"  
+                src="/storage/{{ $profile->image }}" alt=""> 
+                <img class="rounded-circle mx-auto" width="75" height="75" 
+                src="/storage/{{ $profile->image }}" alt=""> 
+                <div class="card-body" style="text-align: center">
+                    <strong>{{$user->name}}</strong><br>
+                    <span>You have <strong>{{$numPosts}}</strong> posts</span>
+                    <div class="pt-3">{{ $profile->description }}</div>
+                    <div class="pt-3"> 
+                        <a href="/profile/edit">Edit profile</a>
+                    </div>
+                </div>  
+            </div> 
+        </div> 
 
-        <div class="row pt-5">
+        <div class="col-md-9 row pt-5">
             @foreach ($posts as $post)
-                <div class="col-4 mb-5">
+            <div class="card">
+                <div class="mb-5 row pt-2">
+                <h5>{{ $post->caption }}</h5>
+                <hr class="solid">
                     <a href="/post/{{$post->id}}">
                         <img src="/storage/{{$post->image}}" class="w-100">
                     </a>
                 </div>
+            </div>
             @endforeach
         </div>
 
